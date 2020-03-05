0e300662c693f5648ad4f8eb3640a088d92b2526
Carlos <kaitoartz@hotmail.com>
2020-03-05 17:54:23 -0600

    terminando con la pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
 $(function(){
   console.log("Se cargo el DOM");
 })
+$("a").click(function(event) {
+  /* Con este if se asegura que this.hash tenga un valor antes de anular el
+comportamiento predeterminado */
+  /* Ahora guardamos y recargamos el navegador y podemos ver que se genera el scroll suave entre
+  secciones. */
+    if (this.hash !== "") {
+      // Previene el comportamiento de click predeterminado
+      event.preventDefault();
+      // Guarda el valor del hash en una variable llamada gato
+      var gato = this.hash;
+      // Usa el método animate para animar el scroll y hacerlo de una forma suave
+      // El número opciona (800) especifica el número de milisegundos que se demorará en llegar hasta el área
+$('html, body').animate({
+scrollTop: $(gato).offset().top
+}, 700, function(){
+// Agrega hash (#) a la URL cuando haya terminado de desplazarse (comportamiento de click predeterminado)
+    window.location.hash = gato;
+    });
+  } // Fin del if
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
